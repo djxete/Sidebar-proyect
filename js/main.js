4e1537195686ce694ddfd4aa64e7c4df499c52d1
Ignacio Herráez <ignacio_hg_87@hotmail.com>
2020-12-09 13:40:33 +0100

    i have finished the sidebar proyect

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-alert("testing");
+//============= VARIABLES ========
+ const btn = document.querySelector(".sidebar-toggle");
+ const btnClose = document.querySelector(".close-btn");
+ const sidebar= document.querySelector(".sidebar");
+ 
+
+
+ //============= ADD EVENT LISTENERS ========
+
+ btn.addEventListener("click", ()=>{
+    sidebar.classList.toggle("show-sidebar");
+    pressBtnClose();
+
+
+ })
+
+
+ 
+
+ //============= ADD EVENT LISTENERS ======== (btnClose)
+
+const pressBtnClose = function(){
+    btnClose.addEventListener("click", ()=>{
+        if(sidebar.classList.contains("show-sidebar")){
+            sidebar.classList.remove("show-sidebar")
+        }
+    })
+}
